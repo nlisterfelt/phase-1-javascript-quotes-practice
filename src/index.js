2f87080087f3b6e3efa5d9fb703383be932025cb
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-19 14:01:44 -0500

    delete and like buttons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded',()=>{
     fetch('http://localhost:3000/quotes?_embed=likes')
     .then(resp=>resp.json())
     .then(data=>data.forEach(quote=>createCard(quote)))
+
+    const form = document.querySelector('#new-quote-form')
+    form.addEventListener('submit', e=>{
+        e.preventDefault()
+        newQuote(e.target)
+    })
 })
 
 function createCard(quoteInfo){
@@ -9,6 +15,7 @@ function createCard(quoteInfo){
     const quoteLI = document.createElement('li')
 
     const blockquote = document.createElement('blockquote')
+    blockquote.id = quoteInfo.id
     blockquote.className = 'blockquote'
 
     const p = document.createElement('p')
@@ -35,4 +42,47 @@ function createCard(quoteInfo){
     blockquote.append(p, footer, br, buttonSuccess, buttonDanger)
     quoteLI.appendChild(blockquote)
     quoteList.appendChild(quoteLI)
+
+    buttonDanger.addEventListener('click', e=>{
+        e.preventDefault()
+        deleteButton(e.target.parentNode)
+    })
+
+    buttonSuccess.addEventListener('click', e=>{
+        e.preventDefault()
+        likeButton(e.target.parentNode)
+    })
+}
+
+function newQuote(info){
+    const quoteObj = {'quote': info.elements[0].value, 'author': info.elements[1].value}
+    createCard({...quoteObj, 'likes': []})
+    fetch('http://localhost:3000/quotes', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(quoteObj)
+    })
+}
+
+function deleteButton(info){
+    const quoteToDelete = document.getElementById(info.id).parentNode
+    quoteToDelete.remove()
+    fetch(`http://localhost:3000/quotes/${info.id}`, {
+        method: 'DELETE',
+        headers: {'content-type': 'application/json'},
+    })
+
+}
+
+function likeButton(info){
+    const quoteLiked = document.getElementById(info.id).querySelector('span')
+    quoteLiked.innerText = parseInt(quoteLiked.innerText)+1
+    const id = parseInt(info.id)
+    console.log(info)
+    fetch('http://localhost:3000/likes', {
+        method: 'POST', 
+        headers: {'content-type': 'application.json'},
+        body: JSON.stringify({'quoteId': id})
+    })
+
 }
